4caeee7de80bd9af58653cb1a71a264de2ab9567
pedroporo <pedroguillferri9@gmail.com>
2024-09-30 19:31:15 +0200

    a

---
+++ php/src/tema3/ej1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Selecció de productes</title>
+</head>
+<body>
+    <h1>Afegir productes al carret</h1>
+    <form action="carret.php" method="POST">
+        <label for="producte">Tria un producte:</label>
+        <select name="producte" id="producte">
+            <option value="Poma">Poma</option>
+            <option value="Plàtan">Plàtan</option>
+            <option value="Taronja">Taronja</option>
+        </select>
+        <input type="submit" value="Afegir al carret">
+    </form>
+    <a href="carret.php">Veure carret</a>
+</body>
+</html>
